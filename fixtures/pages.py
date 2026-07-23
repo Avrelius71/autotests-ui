115af4b65fdb7d86ee66a0d1116f66a668c1a15b
Avrelius71 <artem.dryamin_96@mail.ru>
2026-07-24 00:01:57 +0300

    Практикуемся в реализации PageComponent. Практикуемся в написании компонентов с использованием паттерна PageComponent

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -18,6 +18,11 @@ def registration_page(chromium_page: Page) -> RegistrationPage:
 
 
 @pytest.fixture
+def dashboard_page(chromium_page: Page) -> DashboardPage:
+    return DashboardPage(page=chromium_page)
+
+
+@pytest.fixture
 def dashboard_page_with_state(chromium_page_with_state: Page) -> DashboardPage:
     return DashboardPage(page=chromium_page_with_state)
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,28 @@
 import pytest
-from playwright.sync_api import sync_playwright, Page, Playwright
+from playwright.sync_api import Page, Playwright
 pytest_plugins = (
     "fixtures.pages" # Подключаем фикстуры страниц
 )
 
 
 @pytest.fixture(scope='session')
-def initialize_browser_state():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        user_name_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        email_input.fill('testtest@ya.ru')
-        user_name_input.fill('test')
-        password_input.fill('testpassword')
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-        context.storage_state(path="browser-state.json")
+def initialize_browser_state(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    user_name_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    email_input.fill('testtest@ya.ru')
+    user_name_input.fill('test')
+    password_input.fill('testpassword')
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
+    context.storage_state(path="browser-state.json")
+    page.close()
+    context.close()
+    browser.close()
 
 
 @pytest.fixture
